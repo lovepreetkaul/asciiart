c56b503e6cb51867c43acd6c3559ea6a8bbebabe
Lovepreet Singh <lovepreetkaul23@gmail.com>
2021-11-20 23:20:00 +0530

    Implement print to file for better viewing of large images. Close #1

--- sample/Test.rb
+++ sample/Test.rb
@@ -2,7 +2,7 @@ require_relative "../lib/artascii"
 require "mini_magick"
 
 path = "/home/lovepreet/Downloads/daisy.jpg"
-image_mx = Artascii::Image.new(path, "80x80")
+image_mx = Artascii::Image.new(path)
 puts "Successfully Loaded Image!"
 puts "Image Size: #{image_mx.height} x #{image_mx.width}"
 
@@ -18,4 +18,7 @@ puts "Using average method"
 bright_pixels = image_mx.get_brightness_mx(pixels)
 puts "Brightness Pixel Matrix Size: #{bright_pixels.length} x #{bright_pixels[0].length}"
 
-image_mx.get_ascii_mx(bright_pixels)
+mx = image_mx.get_ascii_mx(bright_pixels)
+image_mx.print_to_file(mx, "matrix.txt")
+
+
